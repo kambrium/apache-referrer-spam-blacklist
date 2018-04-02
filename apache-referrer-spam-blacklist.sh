e1de3e1d2ec34b5358a6dda0cd976823c4de9111
kambrium <kambrium@users.noreply.github.com>
2018-04-02 12:57:56 +0200

    Deleted comment

--- apache-referrer-spam-blacklist.sh
+++ apache-referrer-spam-blacklist.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# This script generates a .htaccess file that blacklists the referrer spammers from https://github.com/matomo-org/referrer-spam-blacklist
-
 echo "RewriteEngine On" > .htaccess
 sort ../referrer-spam-blacklist/spammers.txt | uniq | sed 's/\./\\\\./g' | while read host; 
 do 
